e17278ab8734b2db94b6092c50de6990c4060b12
Obelq <georgi.engineer@gmail.com>
2017-05-25 19:33:46 +0300

    ListLabSolved

---
+++ 2. Linear-Data-Structures/Lists/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //ArrayList<int> list = new ArrayList<int>();
+
+        //list.Add(1);
+        //list.RemoveAt(0);
+
+        //Console.WriteLine(list.Count);
+    }
+}
