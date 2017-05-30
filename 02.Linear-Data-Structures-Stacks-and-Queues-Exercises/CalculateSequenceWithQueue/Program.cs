65a9f4d907ddb020dea83f41ce11d0ea0f4c7fa2
Obelq <georgi.engineer@gmail.com>
2017-05-31 00:28:02 +0300

    StacksAndQueuesExercise

---
+++ 02.Linear-Data-Structures-Stacks-and-Queues-Exercises/CalculateSequenceWithQueue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateSequenceWithQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var magicSequence = new Queue<int>();
+            var seqNumbers = new List<int>
+                    {
+                        n
+                    };
+
+            magicSequence.Enqueue(n);
+            while (seqNumbers.Count < 50)
+            {
+                var firstItem = magicSequence.Peek() + 1;
+                magicSequence.Enqueue(firstItem);
+                var secondItem = (magicSequence.Peek() * 2) + 1;
+                magicSequence.Enqueue(secondItem);
+                var thirdItem = firstItem + 1;
+                magicSequence.Enqueue(thirdItem);
+
+                magicSequence.Dequeue();
+                seqNumbers.Add(firstItem);
+                seqNumbers.Add(secondItem);
+                seqNumbers.Add(thirdItem);
+            }
+
+            Console.WriteLine(string.Join(", ", seqNumbers.Take(50)));
+        }
+    }
+
+}
+
+
---
+++ 02.Linear-Data-Structures-Stacks-and-Queues-Exercises/ReverseNumbersWithAStack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseNumbersWithAStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine()
+                .Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+            Stack<int> stack = new Stack<int>();
+            foreach (var num in nums)
+            {
+                stack.Push(int.Parse(num));
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+        }
+    }
+}
