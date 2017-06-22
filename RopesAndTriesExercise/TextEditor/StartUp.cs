9058cb985e670935de97116a1b364b5f83c0529d
Obelq <georgi.engineer@gmail.com>
2017-06-22 14:23:11 +0300

    Ropes and Tries Lab and Exercise

---
+++ RopesAndTriesExercise/TextEditor/StartUp.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+namespace TextEditor
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            TextEditor textEditor = new TextEditor();
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] inputs = command.Split();
+
+                try
+                {
+                    if (inputs[0] == "login")
+                    {
+                        textEditor.Login(inputs[1]);
+                        continue;
+                    }
+
+                    if (inputs[0] == "users")
+                    {
+                        if (inputs.Length > 1)
+                        {
+                            foreach (string user in textEditor.Users(inputs[1]))
+                            {
+                                Console.WriteLine(user);
+                            }
+                        }
+                        else
+                        {
+                            foreach (string user in textEditor.Users())
+                            {
+                                Console.WriteLine(user);
+                            }
+                        }
+
+                        continue;
+                    }
+
+                    if (inputs[0] == "logout")
+                    {
+                        textEditor.Logout(inputs[1]);
+                        continue;
+                    }
+
+                    if (inputs[1] == "prepend")
+                    {
+                        string[] stringToPrepend = command.Split(new char[] { '"' }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Take(1).ToArray();
+                        textEditor.Prepend(inputs[0], stringToPrepend[0]);
+                        continue;
+                    }
+
+                    if (inputs[1] == "print")
+                    {
+                        Console.WriteLine(textEditor.Print(inputs[0]));
+                        continue;
+                    }
+
+                    if (inputs[1] == "delete")
+                    {
+                        textEditor.Delete(inputs[0], int.Parse(inputs[2]), int.Parse(inputs[3]));
+                        continue;
+                    }
+
+                    if (inputs[1] == "delete")
+                    {
+                        textEditor.Delete(inputs[0], int.Parse(inputs[2]), int.Parse(inputs[3]));
+                        continue;
+                    }
+
+                    if (inputs[1] == "insert")
+                    {
+                        string[] stringToInsert = command.Split(new char[] { '"' }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Take(1).ToArray();
+                        textEditor.Insert(inputs[0], int.Parse(inputs[2]), stringToInsert[0]);
+                        continue;
+                    }
+
+                    if (inputs[1] == "length")
+                    {
+                        Console.WriteLine(textEditor.Length(inputs[0]));
+                        continue;
+                    }
+
+                    if (inputs[1] == "clear")
+                    {
+                        textEditor.Clear(inputs[0]);
+                        continue;
+                    }
+
+                    if (inputs[1] == "substring")
+                    {
+                        textEditor.Substring(inputs[0], int.Parse(inputs[2]), int.Parse(inputs[3]));
+                        continue;
+                    }
+                    if (inputs[1] == "undo")
+                    {
+                        textEditor.Undo(inputs[0]);
+                        continue;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ RopesAndTriesExercise/TextEditor/TextEditor.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+namespace TextEditor
+{
+    class TextEditor : ITextEditor
+    {
+        private Trie<BigList<char>> usersStrings;
+        private Trie<Stack<string>> usersStack;
+
+        public TextEditor()
+        {
+            this.usersStrings = new Trie<BigList<char>>();
+            this.usersStack = new Trie<Stack<string>>();
+        }
+
+
+        public void Login(string username)
+        {
+            this.usersStrings.Insert(username, new BigList<char>());
+            this.usersStack.Insert(username, new Stack<string>());
+        }
+
+        public void Logout(string username)
+        {
+            this.usersStrings.Delete(username);
+            this.usersStack.Delete(username);
+        }
+        public string Print(string username)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return "";
+            }
+            return string.Join("", this.usersStrings.GetValue(username));
+        }
+        public void Prepend(string username, string str)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+
+            this.usersStack.GetValue(username).Push(this.Print(username));
+            this.usersStrings.GetValue(username).AddRangeToFront(str);
+        }
+
+        public void Clear(string username)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+
+            this.usersStack.GetValue(username).Push(this.Print(username));
+            this.usersStrings.GetValue(username).Clear();
+        }
+
+        public void Delete(string username, int startIndex, int length)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+            this.usersStack.GetValue(username).Push(this.Print(username));
+            this.usersStrings.GetValue(username).RemoveRange(startIndex, length);
+        }
+
+        public void Insert(string username, int index, string str)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+
+            this.usersStack.GetValue(username).Push(this.Print(username));
+            this.usersStrings.GetValue(username).InsertRange(index, str);
+        }
+
+        public void Undo(string username)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+            var userString = this.usersStrings.GetValue(username);
+            var userHistory = this.usersStack.GetValue(username);
+            if (userHistory.Count == 0)
+            {
+                return;
+            }
+            var lastUserString = userHistory.Pop();
+            userHistory.Push(this.Print(username));
+            
+            userString.Clear();
+            userString.AddRangeToFront(lastUserString);
+        }
+
+        public void Substring(string username, int startIndex, int length)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return;
+            }
+            this.usersStack.GetValue(username).Push(this.Print(username));
+
+            var userString = this.usersStrings.GetValue(username);
+            var substr = this.usersStrings.GetValue(username).GetRange(startIndex, length);
+            userString.Clear();
+            userString.AddRangeToFront(substr);
+
+        }
+        
+        public int Length(string username)
+        {
+            if (!this.usersStrings.Contains(username))
+            {
+                return -1;
+            }
+            return this.usersStrings.GetValue(username).Count;
+        }
+        
+        public IEnumerable<string> Users(string prefix = "")
+        {
+           return this.usersStrings.GetByPrefix(prefix);
+        }
+    }
+}
